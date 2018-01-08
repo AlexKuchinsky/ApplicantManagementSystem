08e587dca580968cb4d62e34a980ca2ca6b60a59
AlexKuchinsky <alexkuchinskydev@gmail.com>
2018-01-08 21:41:21 +0300

    Worked on tree view
    Made simple, one-level, yet, dinymic tree view. To say truth, the most
    hard and unpleasant work for me is done: using css I defined classes
    which is responsible for displaying three nodes, leaves, connecting
    routes, etc. Also I found, not wrote, function that will handle clicks
    on tree nodes.
    However I managed to write method, wich sends request to the server
    (JSON array, which represents unique "path" of  clicked element), and
    gets response as an JSON array of children of this element. This
    function also builds new nodes and adds them to container.
    A lot of work should be done on the server side. I use reflection to
    "walk" througth objects and their collections recursively and get
    necessary information. Still there are a lot of tricky places, where
    you can get lost easily.

--- Introduction Task/EntrantsManagementSystem/EntrantsManagementSystem/Controllers/UniversitiesController.cs
+++ Introduction Task/EntrantsManagementSystem/EntrantsManagementSystem/Controllers/UniversitiesController.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using EntrantsManagementSystem.Models; 
+using EntrantsManagementSystem.Models;
+using System.Web.Script.Serialization;
+using System.Reflection;
+using System.Collections;
+
 namespace EntrantsManagementSystem.Controllers
 {
     public class UniversitiesController : Controller
     {
-        public UniversitiesDatabaseEntities db = new UniversitiesDatabaseEntities();
+        class Root
+        {
+            public UniversitiesDatabaseEntities db = new UniversitiesDatabaseEntities();
+            public IList Universities => db.Universities.ToList();
+            public IList Entrants { get; set; }
+        }
+        public UniversitiesDatabaseEntities db { get; set; } = new UniversitiesDatabaseEntities();
+
+        public object root = new Root();
+
         public ActionResult Index()
         {
             return View();
@@ -34,6 +47,24 @@ namespace EntrantsManagementSystem.Controllers
         {
             return View(); 
         }
-
+        [HttpPost]
+        public JsonResult GetChilds(string route)
+        {
+            List<int> r = (List<int>)new JavaScriptSerializer().Deserialize(route, typeof(List<int>));
+            return GetChildsRecursion(root, r, r.ToList()); 
+        }
+        public JsonResult GetChildsRecursion(object obj, List<int> route,List<int> initialRoute)
+        {
+            List<PropertyInfo> CollectionProps = obj.GetType().GetProperties().Where(p => p.PropertyType.GetInterface("ICollection") != null).ToList();
+            if (CollectionProps.Count == 0)
+                throw new NotImplementedException();
+            IEnumerable resultList = CollectionProps.Select((p, i) =>
+            {
+                List<int> resultRoute = route.ToList();
+                resultRoute.Add(i);
+                return new { Name = p.Name, NumberOfChildren = (p.GetValue(obj) as ICollection)?.Count ?? 0, Route = resultRoute };
+            }).ToList();
+            return Json(resultList);
+        }
     }
 }
