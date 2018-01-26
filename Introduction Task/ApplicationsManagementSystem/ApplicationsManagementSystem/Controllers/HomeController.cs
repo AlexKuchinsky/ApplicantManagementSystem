f50b72c73fde606d98eaa469438f51f792e73bf8
AlexKuchinsky <alexkuchinskydev@gmail.com>
2018-01-26 22:31:15 +0300

    Applications management system Version 1.0
    1) Created  database.
    2) Filled database with information
    3) Created empty template of project and checked that DB connection
    works perfectly.
    4) Added 4 dropdowns to check, whether tables in database have rigth
    connections. Everything connected remarkably.

---
+++ Introduction Task/ApplicationsManagementSystem/ApplicationsManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ApplicationsManagementSystem.Models;
+namespace ApplicationsManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        UniversitySpecialtiesDatabaseEntities db = new UniversitySpecialtiesDatabaseEntities();
+        public ActionResult Index()
+        {
+            return View("AddSpeciality", db.UniversitySpecialities.Find(2));
+        }
+    }
+}
---
+++ Introduction Task/ApplicationsManagementSystem/ApplicationsManagementSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApplicationsManagementSystem.MvcApplication" Language="C#" %>
